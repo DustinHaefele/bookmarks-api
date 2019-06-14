3d0ee81150c7bd6b79320c27e4effd5e2654a6e4
Dustin Haefele <Dustin.haefele@gmail.com>
2019-06-14 19:48:15 -0400

    server running as expected with client

--- src/bookmarkRoute.js
+++ src/bookmarkRoute.js
@@ -11,14 +11,32 @@ bookmarkRouter
   })
   .post('/', (req, res) => {
     const { title, url, rating, description } = req.body;
+    if(!title || !url || !rating){
+      logger.error('invalid bookmark');
+      return res.status(400).json({message: "Invalid Input"});
+    }
     const myRating = parseInt(rating);
-    const newBookmark = { title, url, rating: myRating, description, id: uuid() };
+
+    const newBookmark = {
+      title,
+      url,
+      rating: myRating,
+      description,
+      id: uuid()
+    };
     bookmarks.push(newBookmark);
     res.status(201).json(newBookmark);
   });
 
-// bookmarkRouter.delete('/:id', (req, res) => {
-//   //FUNCTIONALITY
-// });
+bookmarkRouter.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    logger.error('Invalid bookmark id');
+    return res.status(400).send('Invalid data');
+  }
+  const index = bookmarks.findIndex(b => b.id === id);
+  bookmarks.splice(index, 1);
+  res.status(202).json(bookmarks);
+});
 
 module.exports = bookmarkRouter;
